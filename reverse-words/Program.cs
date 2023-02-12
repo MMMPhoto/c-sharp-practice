77a0d2e035b6dc08feb2b2066d25fb82364f4b01
Max McDonough <max.mcdonough@gmail.com>
2023-02-11 22:39:09 -0500

    Reverse words

---
+++ reverse-words/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class ReverseWords {
+  static void Main() {
+    static string ReverseWords(string str) {
+      char[] arr = str.ToCharArray(0, str.Length);
+      string newString = "";
+      string tempString = "";
+      foreach (char ch in arr) {
+        if (ch == ' ' ) {
+          char[] tempArr = tempString.ToCharArray();
+          Array.Reverse(tempArr);
+          newString += new string(tempArr);
+          newString += ch.ToString();
+          tempString = "";
+        } else {
+          tempString += ch.ToString();
+        }
+      }
+      char[] lastTempArr = tempString.ToCharArray();
+      Array.Reverse(lastTempArr);
+      newString += new string(lastTempArr);
+      return newString;
+    }
+    Console.WriteLine(ReverseWords("This is an example!"));
+  }
+}
+
+
