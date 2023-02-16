874a4fedacf6f5a5c3265473cbe260c506482b68
Max McDonough <max.mcdonough@gmail.com>
2023-02-16 13:27:14 -0500

    rot13

---
+++ rot13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+
+class Rot13 {
+  static void Main() {
+    static string Rot13(string message)
+    {
+      string decoded = "";
+      string code = "abcdefghijklmnopqrstuvwxyz";
+      foreach (char ch in message) {
+        bool upper = Char.IsUpper(ch);
+        if (code.Contains(Char.ToLower(ch))) {
+          int i = code.IndexOf(Char.ToLower(ch));
+          i += (i > 12) ? -13 : 13;
+          decoded += upper ? Char.ToUpper(code[i]) : code[i];    
+        } else {
+          decoded += ch;
+        }      
+      }
+    return decoded;
+    }
+    Console.WriteLine(Rot13("test"));
+    Console.WriteLine(Rot13("Test"));
+  }
+}
+
