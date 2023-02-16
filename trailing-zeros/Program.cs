c9657929aa259e502b74db9c2993893c54e4a865
Max McDonough <max.mcdonough@gmail.com>
2023-02-16 12:56:51 -0500

    trailing-zeros complete

---
+++ trailing-zeros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class TrailingZeros {
+  static void Main() {
+    static int TrailingZeros(int n)
+    {
+      int x = 0;
+      int DivByFive(int n) {
+        n = n / 5;
+        x += n;
+        return (n < 5) ? x : DivByFive(n);
+      }
+      return DivByFive(n);
+    }
+    Console.WriteLine(TrailingZeros(5));
+    Console.WriteLine(TrailingZeros(25));
+    Console.WriteLine(TrailingZeros(531));
+  }
+}
+
+
