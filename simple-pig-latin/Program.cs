dcae8e13f69220f9292e80558b8e4f6631e30dcd
Max McDonough <max.mcdonough@gmail.com>
2023-02-17 22:10:45 -0500

    pig latin

---
+++ simple-pig-latin/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class SimplePigLatin {
+  static void Main() {
+    static string PigIt(string str)
+    {
+      string newStr = "";
+      string[] arr  = str.Split(" ");
+      foreach (string st in arr) {
+        if (Char.IsLetter(st[0])) {
+          newStr += st.Substring(1) + st[0] + "ay ";
+        } else {
+          newStr += st;
+        }
+      }
+      return newStr[newStr.Length - 1] == ' ' ? newStr.Remove(newStr.Length -1, 1) : newStr;
+    }
+    Console.WriteLine(PigIt("Pig latin is cool"));
+    Console.WriteLine(PigIt("This is my string"));
+  }
+}
+
+
