5562e42e11c0c32dddf08f2d91e0b2d6775f25ba
Max McDonough <max.mcdonough@gmail.com>
2023-02-18 19:07:40 -0500

    friend cheating

---
+++ friend-cheating/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class FriendCheating {
+  static void Main() {
+    static List<long[]> removNb(long n) {
+      // Get total sum
+	    long sum = Enumerable.Range(1, (int)n).ToArray().Select(i => (long)i).Aggregate((a, b) => a + b);
+      // Create List<long>[] var
+      List<long[]> output = new List<long[]> {};
+      // Check each number 1 to n
+      for (int i = 1; i < (int)n + 1; i++) {
+        decimal pair = ((decimal)sum - (decimal)i) / ((decimal)i + 1);
+        if (pair % 1 == 0 && pair <= (int)n) {
+          output.Add(new long[] { (long)i, (long)pair });          
+        }
+      }
+		  return output;
+	  }
+    Console.WriteLine(removNb(26));
+  }
+}
+
+	
+  
+  
